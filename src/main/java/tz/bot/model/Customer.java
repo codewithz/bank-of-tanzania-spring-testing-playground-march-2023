0a936d6965b8cbe993e9128615c53f168535d481
Zartab M Nakhwa <zartab@codewithz.com>
2023-03-28 14:11:42 +0530

    Repository Tests

--- src/main/java/tz/bot/model/Customer.java
+++ src/main/java/tz/bot/model/Customer.java
@@ -1,5 +1,6 @@
 package tz.bot.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
@@ -11,13 +12,21 @@ public class Customer {
     private UUID id;
 
     @NotBlank
+    @Column(nullable = false)
     private  String name;
 
     @NotBlank
+    @Column(nullable = false,unique = true)
     private String phoneNumber;
 
 
     public Customer() {
+    }
+
+    public Customer(UUID id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
     }
 
     public UUID getId() {
--- src/test/java/tz/bot/repository/CustomerRepositoryTest.java
+++ src/test/java/tz/bot/repository/CustomerRepositoryTest.java
@@ -1,12 +1,23 @@
 package tz.bot.repository;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import tz.bot.model.Customer;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@DataJpaTest(
+        properties = {
+                "spring.jpa.properties.javax.persistence.validation.mode=none"
+        }
+)
 class CustomerRepositoryTest {
 
     @Autowired
@@ -15,7 +26,75 @@ class CustomerRepositoryTest {
     @Test
     void itShouldSelectCustomerByPhoneNumber() {
         //Given
+        UUID id=UUID.randomUUID();
+        String phoneNumber="12345";
+        Customer customer=new Customer(id,"Mary",phoneNumber);
         //When
+
+        underTest.save(customer);
         //Then
+        Optional<Customer> optionalCustomer=underTest.selectCustomerByPhoneNumber(phoneNumber);
+        Customer selectedCustomer=optionalCustomer.get();
+        assertThat(optionalCustomer).isPresent();
+        assertThat(selectedCustomer).isEqualToComparingFieldByField(customer);
+    }
+
+    @Test
+    void itShouldNotSelectCustomerByPhoneNumberWhenPhoneNumberDoesNotExist() {
+        //Given
+        String phoneNumber="54321";
+        //When
+        Optional<Customer> optionalCustomer = underTest.selectCustomerByPhoneNumber(phoneNumber);
+        //Then
+        assertThat(optionalCustomer).isNotPresent();
+    }
+
+    @Test
+    void itShouldSaveCustomer() {
+        //Given
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, "Alex", "00000");
+
+        //When
+        underTest.save(customer);
+        //Then
+
+        Optional<Customer> optionalCustomer=underTest.findById(id);
+        assertThat(optionalCustomer).isPresent();
+        assertThat(optionalCustomer)
+                .isPresent()
+                .hasValueSatisfying(c->{
+                   assertThat(c.getId()).isEqualTo(id);
+                   assertThat(c.getName()).isEqualTo("Alex");
+                   assertThat(c.getPhoneNumber()).isEqualTo("00000");
+                });
+        assertThat(optionalCustomer.get()).isEqualToComparingFieldByField(customer);
+    }
+
+    @Test
+    void itShouldNotSaveCustomerWhenNameIsNull() {
+        //Given
+        UUID id=UUID.randomUUID();
+        Customer customer=new Customer(id,null,"11111");
+        //When
+
+        //Then
+        assertThatThrownBy(()->underTest.save(customer))
+                .hasMessageContaining("not-null property references a null or transient value : tz.bot.model.Customer.name")
+                .isInstanceOf(DataIntegrityViolationException.class);
+
+    }
+
+    @Test
+    void itShouldNotSaveCustomerWhenPhoneNumberIsNull() {
+        //Given
+        UUID id=UUID.randomUUID();
+        Customer customer=new Customer(id,"Alex",null);
+        //When
+
+        //Then
+        assertThatThrownBy(()->underTest.save(customer))
+                .hasMessageContaining("not-null property references a null or transient value : tz.bot.model.Customer.phoneNumber")
+                .isInstanceOf(DataIntegrityViolationException.class);
     }
 }
